269c02b0f70225e7d9debe1894e60bbba7c7111b
Yaroslav <yarikmeisan@yandex.com>
2022-08-15 15:42:58 +0300

    Add print comments

--- auto-wifi-hack.py
+++ auto-wifi-hack.py
@@ -2,8 +2,13 @@ import os
 print('auto wifi hack')
 
 def hack():
+    print('Enabling root terminal')
+    os.system('sudo su')
+    print('Turn off NetworkManager')
     os.system('systemctl stop NetworkManager')
+    print('Killing processes')
     os.system('airmon-ng check kill')
+    print('Checking wifi card')
     os.system('ifconfig')
 
 hack()
